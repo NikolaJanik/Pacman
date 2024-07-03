d62f7757d65e9cd94953fbe98a51e93578371bb7
Nikola Janik <131357328+NikolaJanik@users.noreply.github.com>
2024-07-03 19:09:57 +0200

    Add files via upload

---
+++ src/pacman/GameComponent.java
@@ -0,0 +1,15 @@
+package pacman;
+
+/*
+An auxiliary class whose objects will be used
+to represent moving components in the game.
+ */
+public class GameComponent {
+
+    public int col, row;
+    public int speed;
+    public String direction;
+    public boolean collisionOn = false;
+
+
+}
